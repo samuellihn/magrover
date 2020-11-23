1917e3b40c80aa1bddd67a6988988b19dfb7cda3
samuellihn <samuellihn@gmail.com>
2020-11-22 22:27:42 -0500

    modified rm3100 library to read measurements and print to console

--- rm3100.py
+++ rm3100.py
@@ -1,124 +1,144 @@
-import spidev
-import board
-import busio
-import math
-from digitalio import Direction
-from adafruit_mcp230xx.mcp23017 import MCP23017
+import time
+# Turns a 2 byte long integer into two 1 byte integers
+def int16_to_bytes(value):
 
-i2c = busio.I2C(board.SCL, board.SDA)
+    return divmod(value, 0x100)
 
 
-def to_bytes(integer):
-    return divmod(integer, 0x100)
+# Register datatype containing a read and write address
+class Register:
+
+    def __init__(self, value):
+        self.write = value.to_bytes(1, 'big')
+        self.read = (value + 0x80).to_bytes(1, 'big')
 
 
-def twos_comp(val, bits=12):
-    """compute the 2's complement of int value val"""
-    if (val[0] & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val[0] = val[0] - (1 << bits)        # compute negative value
-    return val[0]
+# Partial register map for the PNI RM3100 Sensor
+class RegisterMap:
 
-class Sensor:
-    def __init__(self, sensor_num, cycle_count=200):
-        # init mcp23017
-        mcp = [0, 0]
-        mcp[0] = MCP23017(i2c, address=0x20 + 0)
-        try:
-            mcp[1] = MCP23017(i2c, address=0x20 + 1)
-        except ValueError:
-            print("Only one MCP detected")
-        mcp_chip_number = math.floor(sensor_num / 16)
-        mcp_pin_number = sensor_num % 16
-        print(f"Pin {mcp_pin_number} at MCP {mcp_chip_number}")
-        self.ssn = mcp[mcp_chip_number].get_pin(mcp_pin_number)
-        self.ssn.direction = Direction.OUTPUT
-        self.ssn.value = True
+    # To read from a register, send the read address and the data will appear on MISO
+    # TO write to a register, send the write address followed by the value to be written
 
-        # open spi bus
-        self.spi = spidev.SpiDev()
-        self.spi.open(0, 0)
-        self.spi.mode = 0
-        self.spi.max_speed_hz = 500
+    # Register Map
 
-        # select chip
-        self.ssn.value = False
+    continuous_measurement = Register(0x01)
+    continuous_data_rate = Register(0x0B)
+    single_measurement = Register(0x00) # Format 0x0zyx0000
+    status = Register(0x34)
 
-        # set cycle count registers
-        self.cycle_count = hex(cycle_count)
-        cycle_bytes = to_bytes(cycle_count)
-        print(cycle_bytes)
+    # Cycle Count Registers
+    x_cycle_msb = Register(0x04)
+    x_cycle_lsb = Register(0x05)
+    y_cycle_msb = Register(0x06)
+    y_cycle_lsb = Register(0x07)
+    z_cycle_msb = Register(0x08)
+    z_cycle_lsb = Register(0x09)
 
-        # [0] MSB [1] LSB
-        # send cycle counts to registers
-        self.spi.xfer([0x04, cycle_bytes[0], cycle_bytes[1], cycle_bytes[0], cycle_bytes[1], cycle_bytes[0], cycle_bytes[1]])
-        self.ssn_switch()
+    # Measurement Registers
+    mx_2 = Register(0x24)
+    mx_1 = Register(0x25)
+    mx_0 = Register(0x26)
+    my_2 = Register(0x27)
+    my_1 = Register(0x28)
+    my_0 = Register(0x29)
+    mz_2 = Register(0x2A)
+    mz_1 = Register(0x2B)
+    mz_0 = Register(0x2C)
 
-        # initiate one measurement of all three axes, bit format 0b0zyx000
-        self.spi.xfer([0x00, 0b0111000])
 
-        # deselect chip
-        self.ssn.value = True
+class RM3100Measurement:
 
-    def ssn_switch(self):
-        self.ssn.value = True
-        self.ssn.value = False
+    x = 0
+    y = 0
+    z = 0
 
-    def read(self):
+    def __init__(self, sensor_data):
+        # Split raw sensor data
+        x_bytes = sensor_data[0:3]
+        y_bytes = sensor_data[3:6]
+        z_bytes = sensor_data[7:9]
 
-        # deselect chip
-        self.ssn.value = True
+        # Converts twos compliment bytearrays to integers
+        x_measurement = int.from_bytes(x_bytes, byteorder='big', signed=True)
+        y_measurement = int.from_bytes(y_bytes, byteorder='big', signed=True)
+        z_measurement = int.from_bytes(z_bytes, byteorder='big', signed=True)
 
-        # poll until status byte returns true
+        # Converts integers to measurements in microtesla +800 to -800
+        self.x = self.to_microtesla(x_measurement)
+        self.y = self.to_microtesla(y_measurement)
+        self.z = self.to_microtesla(z_measurement)
+
+    def to_microtesla(self, measurement):
+        measurement *= 800
+        measurement /= 8388608
+        return measurement
+
+
+class RM3100:
+
+    def __init__(self, select, si):
+        self.registers = RegisterMap()
+        self.select_pin = select
+        self.spi = spi
+
+        self.select_pin.value = True
+
+    # Reads selected axes and returns a Measurement object with x, y, and z values in microteslas
+    def read(self, read_x=True, read_y=True, read_z=True):
+        self.initiate_single_measurement(read_x, read_y, read_z)
+        raw_measurement = self.read_measurement()
+        measurement = RM3100Measurement(raw_measurement)
+        return measurement
+
+    # Sets the cycle count registers
+    def set_cycle_count(self, cycle_x, cycle_y, cycle_z):
+        # Converts the parameters to a bytearray payload
+        cycle_x_bytes = bytearray(int16_to_bytes(cycle_x))
+        cycle_y_bytes = bytearray(int16_to_bytes(cycle_y))
+        cycle_z_bytes = bytearray(int16_to_bytes(cycle_z))
+
+        # Sends the payload to the cycle count registers
+        self.select_pin.value = False
+        self.spi.write(self.registers.x_cycle_msb.write)
+        self.spi.write(cycle_x_bytes)
+        self.spi.write(cycle_y_bytes)
+        self.spi.write(cycle_z_bytes)
+
+        self.select_pin.value = True
+
+    # Initiates a single measurement from the sensor
+    def initiate_single_measurement(self, x=True, y=True, z=True):
+        self.select_pin.value = False
+
+        # Sends value depending on input values
+        measurement_value = 0b00000000
+        if x:
+            measurement_value += 0b00010000
+        if y:
+            measurement_value += 0b00100000
+        if z:
+            measurement_value += 0b01000000
+        self.spi.write(self.registers.single_measurement.write)
+        self.spi.write(measurement_value.to_bytes(1, "big"))
+
+        self.select_pin.value = True
+
+    # Reads a measurement from the sensor and returns a bytearray of length 9
+    # Bytearray uses three bytes for x, y, and z respectively in big endian format
+    def read_measurement(self):
+        # Polls until measurement is ready
+        is_ready = bytearray([0])
         while True:
-            self.ssn.value = False
-            status = self.spi.readbytes(1)[0]
-            print(f"Status is {status}")
-            if status >= 128:
-                # read bytes
-                sensor_data_x = self.spi.readbytes(3)
-                sensor_data_y = self.spi.readbytes(3)
-                sensor_data_z = self.spi.readbytes(3)
+            self.select_pin.value = False
+            self.spi.write_readinto(self.registers.mx_2.read, is_ready)
+            if int.from_bytes(is_ready, 'big') >= 0x80:
                 break
             else:
-                # if measurement is not ready, reset ssn and try again
-                self.ssn.value = True
-                
-        # deselect chip
-        print(self.ssn.value)
-        self.ssn.value = True
-        
-        # return values
-        print(sensor_data_x, sensor_data_y, sensor_data_z)
-        values = self.sensor_data_xyz(sensor_data_x, sensor_data_y, sensor_data_z)
-        print(values)
-        return values
-    
-    def process_sensor_data(self, sensor_data):
-        raw_value = twos_comp(sensor_data)
-        value_in_ut = raw_value / 8388608 * 800
-        return value_in_ut
+                self.select_pin.value = True
 
-    def sensor_data_xyz(self, sensor_data_x, sensor_data_y, sensor_data_z):
-        x_ut = self.process_sensor_data(sensor_data_x)
-        y_ut = self.process_sensor_data(sensor_data_y)
-        z_ut = self.process_sensor_data(sensor_data_z)
-        return x_ut, y_ut, z_ut
-    
-    def initiate_new_measurement(self):
-        self.ssn.value = False
-        # initiate one measurement of all three axes, bit format 0b0zyx000
-        self.spi.xfer([0x00, 0b0111000])
-        self.ssn.value = True
-
-#   Use this method for normal read
-    def read_and_initiate(self):
-        mx, my, mz = self.read()
-        self.initiate_new_measurement()
-        return mx, my, mz
-        
-
-
-
-
-
+        # Reads values
+        read_buffer = bytearray([0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.spi.readinto(read_buffer)
+        self.select_pin.value = True
+        return read_buffer
 
--- rm3100test.py
+++
@@ -1,8 +0,0 @@
-from rm3100 import Sensor
-import time
-
-
-mag = Sensor(0)
-while 1:
-    print(mag.read_and_initiate())
-    time.sleep(2)
